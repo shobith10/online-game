5c833da2a23575827758fda45ac5f5cf1c783a6b
shobith p <45303620+shobith123@users.noreply.github.com>
2020-02-13 18:09:29 +0530

    Update scripted.js

--- scripted.js
+++ scripted.js
@@ -39,64 +39,64 @@ function play()
     var op=document.getElementById("sel").value;
     if(op=="1")
     {
-        document.getElementById("player").src="images/one.png";
+        document.getElementById("player").src="one.png";
         runp= runp+1;
     }
     if(op=="2")
     {
-        document.getElementById("player").src="images/two.png";
+        document.getElementById("player").src="two.png";
         runp= runp+2;
     }
     if(op=="3")
     {
-        document.getElementById("player").src="images/three.png";
+        document.getElementById("player").src="three.png";
         runp= runp+3;
     }
     if(op=="4")
     {
-        document.getElementById("player").src="images/four.png";
+        document.getElementById("player").src="four.png";
         runp= runp+4;
     }
     if(op=="5")
     {
-        document.getElementById("player").src="images/five.png";
+        document.getElementById("player").src="five.png";
         runp= runp+5;
     }
     if(op=="6")
     {
-        document.getElementById("player").src="images/six.png";
+        document.getElementById("player").src="six.png";
         runp= runp+6;
     }
     
 
     if(no==6)
     {
-        pic.src="images/six.png";
+        pic.src="six.png";
         runc=runc+6;
     }
     else if(no==5)
     {
-    pic.src="images/five.png";
+    pic.src="five.png";
     runc=runc+5;
     }
     else if(no==4)
     {
-    pic.src="images/four.png";
+    pic.src="four.png";
     runc=runc+4;
     }
     else if(no==3)
     {
-    pic.src="images/three.png";
+    pic.src="three.png";
     runc=runc+3;
     }
     else if(no==2)
     {
-    pic.src="images/two.png";
+    pic.src="two.png";
     runc=runc+2;
     }
     else if(no==1)
     {
-    pic.src="images/one.png";
+    pic.src="one.png";
     runc=runc+1;
     }
 
